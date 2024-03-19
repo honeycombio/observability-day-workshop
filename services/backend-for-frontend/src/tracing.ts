191658b3e4eaaddf2cd18968ff4286cd29cbbc82
Jessica Kerr <jessitron@honeycomb.io>
2024-03-19 17:21:32 -0500

    Reveal the tracing problem

--- services/backend-for-frontend/src/tracing.ts
+++ services/backend-for-frontend/src/tracing.ts
@@ -1,7 +1,12 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import * as opentelemetry from '@opentelemetry/api';
 
+opentelemetry.diag.setLogger(
+    new opentelemetry.DiagConsoleLogger(),
+    opentelemetry.DiagLogLevel.INFO
+);
 // The Trace Exporter exports the data to Honeycomb and uses
 // environment variables for endpoint, service name, and API Key.
 const traceExporter = new OTLPTraceExporter();
