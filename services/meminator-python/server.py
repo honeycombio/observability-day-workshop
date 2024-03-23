889c4f25ddef751d5f2a9f7d94647c82e20c54b0
Jessica Kerr <jessitron@honeycomb.io>
2024-03-23 17:57:41 -0500

    feat: it serves BusinessWitch

--- services/meminator-python/server.py
+++ services/meminator-python/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, jsonify, send_file
 
 app = Flask(__name__)
 # Route for health check
@@ -6,9 +7,21 @@ app = Flask(__name__)
 def health():
     return jsonify({"message": "I am here", "status_code": 0})
 
-@app.route('/applyPhraseToPicture', methods=['POST'])
+@app.route('/applyPhraseToPicture', methods=['POST', 'GET'])
 def meminate():
-    return
+
+    # Get the absolute path to the PNG file
+    image_path = os.path.abspath('tmp/BusinessWitch.png')
+
+    # Check if the file exists
+    if not os.path.exists(image_path):
+        return 'Backup image file not found', 500
+
+    # Serve the PNG file
+    return send_file(
+        image_path,
+        mimetype='image/png'
+    )
 
 if __name__ == '__main__':
     app.run(debug=True, port=10114)
