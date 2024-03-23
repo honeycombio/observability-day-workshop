90360b31e8bf6a694cf2b92e9d9be0ea146b591c
Jessica Kerr <jessitron@honeycomb.io>
2024-03-23 18:01:09 -0500

    feat: it applies a text

--- services/meminator-python/server.py
+++ services/meminator-python/server.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from flask import Flask, jsonify, send_file
 
 app = Flask(__name__)
@@ -11,15 +12,32 @@ def health():
 def meminate():
 
     # Get the absolute path to the PNG file
-    image_path = os.path.abspath('tmp/BusinessWitch.png')
+    input_image_path = os.path.abspath('tmp/BusinessWitch.png')
 
     # Check if the file exists
-    if not os.path.exists(image_path):
+    if not os.path.exists(input_image_path):
         return 'Backup image file not found', 500
+    
+    # Define the text to apply
+    text = "I got you"
+
+    # Define the output image path
+    output_image_path = os.path.abspath('tmp/BW_with_text.png')
+
+    command = ['convert', input_image_path, '-fill', 'white', '-pointsize', '36', '-gravity', 'center', '-annotate', '0', text, output_image_path]
+    # Execute ImageMagick command to apply text to the image
+    subprocess.run(command)
+
+    # Serve the modified image
+    return send_file(
+        output_image_path,
+        mimetype='image/png'
+    )
+
 
     # Serve the PNG file
     return send_file(
-        image_path,
+        input_image_path,
         mimetype='image/png'
     )
 
