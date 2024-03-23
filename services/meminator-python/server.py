95e62bf2d8e4bcf145fe1ffa8e968a79e8066d5a
Jessica Kerr <jessitron@honeycomb.io>
2024-03-23 18:22:15 -0500

    feat (broken): give it the imagemagick args from node

--- services/meminator-python/server.py
+++ services/meminator-python/server.py
@@ -1,6 +1,10 @@
 import os
 import subprocess
+import uuid
 from flask import Flask, jsonify, send_file
+
+IMAGE_MAX_WIDTH_PX=1000
+IMAGE_MAX_HEIGHT_PX=1000
 
 app = Flask(__name__)
 # Route for health check
@@ -22,11 +26,25 @@ def meminate():
     text = "I got you"
 
     # Define the output image path
-    output_image_path = os.path.abspath('tmp/BW_with_text.png')
+    output_image_path = generate_random_filename(input_image_path)
 
-    command = ['convert', input_image_path, '-fill', 'white', '-pointsize', '36', '-gravity', 'center', '-annotate', '0', text, output_image_path]
+    command = ['convert',
+        '-resize', f'{IMAGE_MAX_WIDTH_PX}x{IMAGE_MAX_HEIGHT_PX}>',
+        '-gravity', 'North',
+        '-pointsize', '48',
+        '-fill', 'white',
+        '-undercolor', '#00000080',
+        '-font', 'Angkor-Regular',
+        '-annotate', '0', text, output_image_path]
     # Execute ImageMagick command to apply text to the image
-    subprocess.run(command)
+    result = subprocess.run(command)
+
+    if result.returncode == 0:
+        print("Subprocess completed successfully.")
+    else:
+        print("Subprocess failed with return code:", result.returncode)
+        # Access stderr output
+        print("Error output:", result.stderr)
 
     # Serve the modified image
     return send_file(
@@ -34,12 +52,22 @@ def meminate():
         mimetype='image/png'
     )
 
+def generate_random_filename(input_filename):
+    # Extract the extension from the input filename
+    _, extension = os.path.splitext(input_filename)
+    
+    # Generate a UUID and convert it to a string
+    random_uuid = uuid.uuid4()
+    # Convert UUID to string and remove dashes
+    random_filename = str(random_uuid).replace("-", "")
+    
+    # Append the extension to the random filename
+    random_filename_with_extension = random_filename + extension
+    
+    random_filepath = os.path.join("/tmp", random_filename_with_extension)
+    
+    return random_filepath
 
-    # Serve the PNG file
-    return send_file(
-        input_image_path,
-        mimetype='image/png'
-    )
 
 if __name__ == '__main__':
     app.run(debug=True, port=10114)
