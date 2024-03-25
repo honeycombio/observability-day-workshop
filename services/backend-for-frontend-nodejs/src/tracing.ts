1a928cbf55ad73d32e283b6a19ab8d53a88d6874
Jessica Kerr <jessitron@honeycomb.io>
2024-03-25 16:30:54 -0700

    Workshop prep: comment out the diag logger

--- services/backend-for-frontend-nodejs/src/tracing.ts
+++ services/backend-for-frontend-nodejs/src/tracing.ts
@@ -3,12 +3,12 @@ import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentation
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import * as opentelemetry from '@opentelemetry/api';
 
-opentelemetry.diag.setLogger(
-    new opentelemetry.DiagConsoleLogger(),
-    opentelemetry.DiagLogLevel.INFO
-);
-// The Trace Exporter exports the data to Honeycomb and uses
-// environment variables for endpoint, service name, and API Key.
+// opentelemetry.diag.setLogger( // INSTR: make it tell you when it fails to send traces
+//     new opentelemetry.DiagConsoleLogger(),
+//     opentelemetry.DiagLogLevel.INFO
+// );
+
+// The Trace Exporter uses environment variables for endpoint, service name, and API Key.
 const traceExporter = new OTLPTraceExporter();
 
 const sdk = new NodeSDK({
