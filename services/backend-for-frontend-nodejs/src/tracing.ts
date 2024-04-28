853628d59b4e4b86e80fecdd26b55a4727f464bb
Jessica Kerr <jessitron@honeycomb.io>
2024-04-28 14:41:44 -0500

    Revert "Add undici instrumentation to make node-fetch show stuff"
    This is so that we can show adding it in the workshop
    
    This reverts commit 710241260b6bbe0f6e41dbbcb54aa723ed5d7da9.

--- services/backend-for-frontend-nodejs/src/tracing.ts
+++ services/backend-for-frontend-nodejs/src/tracing.ts
@@ -2,8 +2,6 @@ import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import * as opentelemetry from '@opentelemetry/api';
-const { UndiciInstrumentation } = require('@opentelemetry/instrumentation-undici');
-
 
 // opentelemetry.diag.setLogger( // INSTRUMENTATION: make it tell you when it fails to send traces
 //     new opentelemetry.DiagConsoleLogger(),
@@ -17,7 +15,7 @@ const sdk = new NodeSDK({
     traceExporter,
     instrumentations: [getNodeAutoInstrumentations(
         //  { '@opentelemetry/instrumentation-fs': { enabled: false } } // INSTRUMENTATION: remove the noisy spans that we don't use
-    ), new UndiciInstrumentation()]
+    )]
 });
 
 sdk.start();
