2e69f65f8c06e3df1b9afb83b5a4695e0f4a8c96
Jessica Kerr <jessitron@honeycomb.io>
2024-05-01 22:14:04 -0500

    Loadgen, in bash

---
+++ scripts/loadgen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set your endpoint URL
+URL="http://localhost:8080/backend/createPicture"
+
+while true; do
+    # Call the endpoint and print the HTTP status code
+    curl -o /dev/null -X POST -w "%{http_code}\n" $URL
+
+    # Sleep for a random time between 1 and 2 seconds
+    sleep $(awk -v min=1 -v max=2 'BEGIN{srand(); print min+rand()*(max-min)}')
+done
