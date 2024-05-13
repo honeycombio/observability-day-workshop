8e2eab4cf125443a70408fb27e9ee570e46d6acb
Jessica Kerr <jessitron@honeycomb.io>
2024-05-13 18:26:25 -0500

    How to add an attribute to the current span

--- services/phrase-picker-java/src/main/java/io/honeydemo/meminator/phrasepicker/controller/PhraseController.java
+++ services/phrase-picker-java/src/main/java/io/honeydemo/meminator/phrasepicker/controller/PhraseController.java
@@ -2,13 +2,17 @@ package io.honeydemo.meminator.phrasepicker.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.opentelemetry.api.trace.Span; // INSTRUMENTATION
 
 @RestController
 public class PhraseController {
 
     @GetMapping("/phrase")
     public PhraseResult hello() {
-        return new PhraseResult("It could be worse");
+        String phrase = "It could be worse";
+        // INSTRUMENTATION: add a useful attribute
+        // Span.current().setAttribute("app.phrase", phrase);
+        return new PhraseResult(phrase);
     }
 
     public static class PhraseResult {
