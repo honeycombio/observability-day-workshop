2139232c80e24a8461602f41567e9610e9f4c140
Jessica Kerr <jessitron@honeycomb.io>
2024-05-13 17:03:36 -0500

    It returns a phrase in json now

---
+++ services/phrase-picker-java/README.md
@@ -0,0 +1,9 @@
+# phrase picker in Java
+
+To run:
+
+```shell
+mvn spring-boot:run
+```
+
+Thank you, copilot. 
---
+++ services/phrase-picker-java/src/main/java/io/honeydemo/meminator/phrasepicker/controller/PhraseController.java
@@ -0,0 +1,29 @@
+package io.honeydemo.meminator.phrasepicker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PhraseController {
+
+    @GetMapping("/phrase")
+    public PhraseResult hello() {
+        return new PhraseResult("It could have been worse");
+    }
+
+    public static class PhraseResult {
+        private String phrase;
+
+        public PhraseResult(String phrase) {
+            this.phrase = phrase;
+        }
+
+        public String getPhrase() {
+            return phrase;
+        }
+
+        public void setPhrase(String phrase) {
+            this.phrase = phrase;
+        }
+    }
+}
