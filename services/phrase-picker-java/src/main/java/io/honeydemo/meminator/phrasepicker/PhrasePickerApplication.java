691803bd87c2ea93e9885a0bd45bcde5041cafca
Jessica Kerr <jessitron@honeycomb.io>
2024-05-13 16:45:23 -0500

    start.spring.io
    with Spring Web and Docker Compose

---
+++ services/phrase-picker-java/src/main/java/io/honeydemo/meminator/phrasepicker/PhrasePickerApplication.java
@@ -0,0 +1,13 @@
+package io.honeydemo.meminator.phrasepicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrasePickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhrasePickerApplication.class, args);
+	}
+
+}
