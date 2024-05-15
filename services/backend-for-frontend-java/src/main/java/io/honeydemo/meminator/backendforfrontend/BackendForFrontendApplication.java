f0b838af7cf348bc010e86f7f70a1741c4181b4d
Jessica Kerr <jessitron@honeycomb.io>
2024-05-14 19:56:11 -0500

    We are trying manual instrumentation

--- services/backend-for-frontend-java/src/main/java/io/honeydemo/meminator/backendforfrontend/BackendForFrontendApplication.java
+++ services/backend-for-frontend-java/src/main/java/io/honeydemo/meminator/backendforfrontend/BackendForFrontendApplication.java
@@ -2,11 +2,20 @@ package io.honeydemo.meminator.backendforfrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import io.opentelemetry.instrumentation.log4j.appender.v2_17.OpenTelemetryAppender;
+
+import io.opentelemetry.sdk.OpenTelemetrySdk;
 
 @SpringBootApplication
 public class BackendForFrontendApplication {
 
 	public static void main(String[] args) {
+
+		OpenTelemetrySdk sdk = AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
+
+		OpenTelemetryAppender.install(sdk);
+
 		SpringApplication.run(BackendForFrontendApplication.class, args);
 	}
 
