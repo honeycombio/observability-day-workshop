a7c832770033b7c5c2e66a204d1477dcf12d3265
Jessica Kerr <jessitron@honeycomb.io>
2024-05-14 21:29:23 -0500

    Revert "It does require the manual initialization"
    This reverts commit c7e1837630ec8581a760804386cc4b0eb18837b2.

--- services/backend-for-frontend-java/src/main/java/io/honeydemo/meminator/backendforfrontend/BackendForFrontendApplication.java
+++ services/backend-for-frontend-java/src/main/java/io/honeydemo/meminator/backendforfrontend/BackendForFrontendApplication.java
@@ -4,21 +4,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
-import io.opentelemetry.instrumentation.log4j.appender.v2_17.OpenTelemetryAppender;
-
-import io.opentelemetry.sdk.OpenTelemetrySdk;
 
 @SpringBootApplication
 public class BackendForFrontendApplication {
 
 	public static void main(String[] args) {
 
-		OpenTelemetrySdk sdk = AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
-
 		SpringApplication.run(BackendForFrontendApplication.class, args);
-
-		OpenTelemetryAppender.install(sdk);
 
 		Logger logger = LogManager.getLogger("poo");
 		logger.info("everything is stupid");
