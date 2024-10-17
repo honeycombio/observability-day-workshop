ae084917512d514811a4f0b776519ad83d990b47
Jessica Kerr <jessitron@honeycomb.io>
2024-10-17 06:30:48 -0700

    Restore original functionality

--- services/backend-for-frontend-nodejs/src/index.ts
+++ services/backend-for-frontend-nodejs/src/index.ts
@@ -1,64 +1,69 @@
-import "./tracing"
-import express, { Request, Response } from 'express';
+import "./tracing";
+import express, { Request, Response } from "express";
 import { fetchFromService } from "./o11yday-lib";
+// import { trace, SpanStatusCode } from "@opentelemetry/api";
 
 const app = express();
 const PORT = 10115;
 app.use(express.json());
 
 app.get("/health", (req: Request, res: Response) => {
-    res.send({ message: "I am here", status_code: 0 });
+  res.send({ message: "I am here", status_code: 0 });
 });
 
-app.post('/createPicture', async (req: Request, res: Response) => {
-  //  const span = trace.getActiveSpan();
-    try {
-        const [phraseResponse, imageResponse] = await Promise.all([
-            fetchFromService('phrase-picker'),
-            fetchFromService('image-picker')
-        ]);
-        const phraseText = phraseResponse.ok ? await phraseResponse.text() : "{}";
-        const imageText = imageResponse.ok ? await imageResponse.text() : "{}";
+app.post("/createPicture", async (req: Request, res: Response) => {
+  // const span = trace.getActiveSpan();
+  try {
+    const [phraseResponse, imageResponse] = await Promise.all([
+      fetchFromService("phrase-picker"),
+      fetchFromService("image-picker"),
+    ]);
+    const phraseText = phraseResponse.ok ? await phraseResponse.text() : "{}";
+    const imageText = imageResponse.ok ? await imageResponse.text() : "{}";
     //    span?.setAttributes({ "app.phraseResponse": phraseText, "app.imageResponse": imageText }); // INSTRUMENTATION: add relevant info to span
-        const phraseResult = JSON.parse(phraseText);
-        const imageResult = JSON.parse(imageText);
+    const phraseResult = JSON.parse(phraseText);
+    const imageResult = JSON.parse(imageText);
 
-        const response = await fetchFromService('meminator', {
-            method: "POST",
-            body: {
-                ...phraseResult, ...imageResult
-            }
-        });
+    const response = await fetchFromService("meminator", {
+      method: "POST",
+      body: {
+        ...phraseResult,
+        ...imageResult,
+      },
+    });
 
-        if (!response.ok) {
-            throw new Error(`Failed to fetch picture from meminator: ${response.status} ${response.statusText}`);
-        }
-        if (response.body === null) {
-            throw new Error(`Failed to fetch picture from meminator: ${response.status} ${response.statusText}`);
-        }
-
-        res.contentType('image/png');
-        // Read the response body as binary data
-        const reader = response.body.getReader();
-        // Stream the chunks of the picture data to the response as they are received
-        while (true) {
-            const { done, value } = await reader.read();
-            if (done) {
-                break;
-            }
-            res.write(value);
-        }
-        res.end()
-
-    } catch (error) {
-  //      span?.recordException(error as Error);
-        console.error('Error creating picture:', error);
-        res.status(500).send('Internal Server Error');
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch picture from meminator: ${response.status} ${response.statusText}`
+      );
     }
-});
+    if (response.body === null) {
+      throw new Error(
+        `Failed to fetch picture from meminator: ${response.status} ${response.statusText}`
+      );
+    }
 
+    res.contentType("image/png");
+    // Read the response body as binary data
+    const reader = response.body.getReader();
+    // Stream the chunks of the picture data to the response as they are received
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+      res.write(value);
+    }
+    res.end();
+  } catch (error) {
+    // span?.recordException(error as Error);
+    // span?.setStatus({ code: SpanStatusCode.ERROR }); // the instrumentation does this on a 500
+    console.error("Error creating picture:", error);
+    res.status(500).send("This is terrible");
+  }
+});
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
