1283ecb17e10e37ff7c483dea4c61f2943c8d60a
Jessica Kerr <jessitron@honeycomb.io>
2025-03-07 08:57:42 -0600

    Prepping for Instrumentation Strategies
    updating the 'production' app

---
+++ scripts/publish-honeydemo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Publish the images needed to deploy from github.com/honeycombio/demo, meminator directory
+# to meminator.honeydemo.io
+
+# set HONEYCOMB_API_KEY_WEB to the one that should be used in the frontend image
+
+set -x
+set -e
+
+# it will read .env
+
+docker compose -f docker-compose-honeydemo.yaml build --push
--- services/web/src/index.js
+++ services/web/src/index.js
@@ -11,44 +11,45 @@
 // Function to fetch the image binary data from the server
 
 async function fetchPicture() {
-    try {
-        // Start with the loading image
-        document.getElementById('picture').style = "display:none";
-        document.getElementById('loading-meme').style = "display:block";
-        document.getElementById('message').innerText = "Generating meme...";
-        document.getElementById('message').style = "display:block";
+  try {
+    // Start with the loading image
+    document.getElementById("picture").style = "display:none";
+    document.getElementById("loading-meme").style = "display:block";
+    document.getElementById("message").innerText = "Generating meme...";
+    document.getElementById("message").style = "display:block";
 
-        const response = await fetch('/backend/createPicture', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            // Optionally, you can send data in the request body if needed
-            // body: JSON.stringify({ /* any data you want to send */ })
-        });
+    const response = await fetch("/backend/createPicture", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Optionally, you can send data in the request body if needed
+      // body: JSON.stringify({ /* any data you want to send */ })
+    });
 
-        if (!response.ok) {
-            throw new Error('Failed to fetch picture');
-        }
-
-        // Convert the binary response to a blob
-        const blob = await response.blob();
-
-        // Create a URL for the blob
-        const imgUrl = URL.createObjectURL(blob);
-
-        // Set the image source to the URL
-        document.getElementById('loading-meme').style = "display:none";
-        document.getElementById('message').style = "display:none";
-        document.getElementById('picture').src = imgUrl;
-        document.getElementById('picture').style = "display:block;";
-    } catch (error) {
-        console.error('Error fetching picture:', error);
-        document.getElementById('loading-meme').style = "display:none";
-        document.getElementById('picture').style = "display:none;";
-        document.getElementById('message').innerText = "There was an error fetching a picture. Please retry.";
-        document.getElementById('message').style = "display:block;";
+    if (!response.ok) {
+      throw new Error("Failed to fetch picture");
     }
+
+    // Convert the binary response to a blob
+    const blob = await response.blob();
+
+    // Create a URL for the blob
+    const imgUrl = URL.createObjectURL(blob);
+
+    // Set the image source to the URL
+    document.getElementById("loading-meme").style = "display:none";
+    document.getElementById("message").style = "display:none";
+    document.getElementById("picture").src = imgUrl;
+    document.getElementById("picture").style = "display:block;";
+  } catch (error) {
+    console.error("Error fetching picture:", error);
+    document.getElementById("loading-meme").style = "display:none";
+    document.getElementById("picture").style = "display:none;";
+    document.getElementById("message").innerText =
+      "There was an error fetching a picture. Please retry.";
+    document.getElementById("message").style = "display:block;";
+  }
 }
 
-document.getElementById('go').addEventListener('click', fetchPicture);
+document.getElementById("go").addEventListener("click", fetchPicture);
