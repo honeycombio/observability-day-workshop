9383e450162484a4f53e10e7b584015c7655b8fe
Jessica Kerr <jessitron@honeycomb.io>
2025-03-09 21:31:14 -0500

    Show the noisy fs instrumentation

--- services/image-picker-nodejs/src/tracing.ts
+++ services/image-picker-nodejs/src/tracing.ts
@@ -1,24 +1,28 @@
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
-import * as opentelemetry from '@opentelemetry/api';
-import { ConfigurationSpanProcessor } from './config';
+import * as opentelemetry from "@opentelemetry/api";
+import { ConfigurationSpanProcessor } from "./config";
 
 opentelemetry.diag.setLogger(
-    new opentelemetry.DiagConsoleLogger(),
-    opentelemetry.DiagLogLevel.INFO
+  new opentelemetry.DiagConsoleLogger(),
+  opentelemetry.DiagLogLevel.INFO
 );
 // The Trace Exporter exports the data to Honeycomb and uses
 // environment variables for endpoint, service name, and API Key.
 const traceExporter = new OTLPTraceExporter();
 
 const sdk = new NodeSDK({
-    // traceExporter, // when you have spanProcessors, this is ignored! Add it to the processors, inside a BatchSpanProcessor.
-    spanProcessors: [new ConfigurationSpanProcessor(), new BatchSpanProcessor(traceExporter)],
-    instrumentations: [getNodeAutoInstrumentations(
-        { '@opentelemetry/instrumentation-fs': { enabled: false } }
-    )]
+  // traceExporter, // when you have spanProcessors, this is ignored! Add it to the processors, inside a BatchSpanProcessor.
+  spanProcessors: [
+    new ConfigurationSpanProcessor(),
+    new BatchSpanProcessor(traceExporter),
+  ],
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+    //  { '@opentelemetry/instrumentation-fs': { enabled: false } }
+  ],
 });
 
 sdk.start();
