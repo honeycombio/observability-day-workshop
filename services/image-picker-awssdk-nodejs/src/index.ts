8a980838c3f3d283e401ccaf1bc87bc89b082b53
Jessica Kerr <jessitron@honeycomb.io>
2025-03-09 20:18:45 -0500

    This doesn't work locally
    I can't get the AWS SDK to use unauthenticated access to the public bucket.
    It fails because it can't figure out who I am on AWS.
    I could fix this for myself locally, but I want this demo to run for other people, and this never will.

--- services/image-picker-awssdk-nodejs/src/index.ts
+++ services/image-picker-awssdk-nodejs/src/index.ts
@@ -1,45 +1,50 @@
-import express, { Request, Response } from 'express';
-import { S3Client, ListObjectsV2Command } from '@aws-sdk/client-s3';
-import { trace } from '@opentelemetry/api';
+import express, { Request, Response } from "express";
+import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
+import { trace } from "@opentelemetry/api";
 
 const app = express();
 const PORT = process.env.PORT || 10118;
 
-const BUCKET_NAME = process.env.BUCKET_NAME || 'random-pictures';
+const BUCKET_NAME = process.env.BUCKET_NAME || "random-pictures";
 
 app.use(express.json());
 
 app.get("/health", (req: Request, res: Response) => {
-    res.send("OK");
+  res.send("OK");
 });
 
-app.get('/imageUrl', async (req: Request, res: Response) => {
-    const s3Client = new S3Client({ region: 'us-east-1' });
-    const input = {
-        Bucket: BUCKET_NAME,
+app.get("/imageUrl", async (req: Request, res: Response) => {
+  const s3Client = new S3Client({
+    region: "us-east-1",
+  });
+  const input = {
+    Bucket: BUCKET_NAME,
+  };
+  const command = new ListObjectsV2Command(input);
+  const response = await s3Client.send(command);
+
+  const images: string[] = [];
+  if (response.Contents) {
+    for await (const image of response.Contents) {
+      images.push(image.Key as string);
     }
-    const command = new ListObjectsV2Command(input);
-    const response = await s3Client.send(command);
+  }
 
-    const images: string[] = [];
-    if (response.Contents) {
-        for await (const image of response.Contents) {
-            images.push(image.Key as string);
-        }
-    }
+  const randomImage = choose(images);
+  trace.getActiveSpan()?.setAttributes({ "app.imageKey": randomImage });
 
-    const randomImage = choose(images)
-    trace.getActiveSpan()?.setAttributes({ "app.imageKey": randomImage });
-
-    res.send({ imageKey: randomImage });
+  res.send({ imageKey: randomImage });
 });
 
 function choose<T>(array: T[]): T {
-    const i = Math.floor(Math.random() * array.length);
-    trace.getActiveSpan()?.setAttributes({ "app.choiceIndex": i, "app.numberOfChoices": array.length }); // INSTRUMENTATION: add relevant info
-    return array[i];
+  const i = Math.floor(Math.random() * array.length);
+  trace.getActiveSpan()?.setAttributes({
+    "app.choiceIndex": i,
+    "app.numberOfChoices": array.length,
+  }); // INSTRUMENTATION: add relevant info
+  return array[i];
 }
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
