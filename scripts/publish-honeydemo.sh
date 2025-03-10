57ec62ecb6d5b62a9afed8c6aa431f1098641d98
Jessica Kerr <jessitron@honeycomb.io>
2025-03-10 15:56:49 -0500

    refactor. don't use this extra env var, confusing

--- scripts/publish-honeydemo.sh
+++ scripts/publish-honeydemo.sh
@@ -3,8 +3,6 @@
 # Publish the images needed to deploy from github.com/honeycombio/demo, meminator directory
 # to meminator.honeydemo.io
 
-# set HONEYCOMB_API_KEY_WEB to the one that should be used in the frontend image
-
 set -x
 set -e
 
