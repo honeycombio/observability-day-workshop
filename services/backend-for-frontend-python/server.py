ec5170f168218d01c97f52c2f1b3a933a757849c
Jessica Kerr <jessitron@honeycomb.io>
2025-04-13 20:48:20 -0500

    Fix htmx implementation to use outerHTML swap - auggie

--- services/backend-for-frontend-python/server.py
+++ services/backend-for-frontend-python/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, Response, request
+from flask import Flask, jsonify, Response, request, render_template_string
 from o11yday_lib import fetch_from_service
 from opentelemetry import trace
 from opentelemetry.trace import SpanKind
@@ -73,8 +73,16 @@ def submit_rating():
 
 @app.route('/user-info', methods=['GET'])
 def user_info():
-    # Just return the username text
-    return "Meminator User"
+    # HTML template for the user info
+    user_info_template = '''
+    <div class="user-info" id="user-info">
+        <img id="user-avatar" src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y" alt="User Avatar" class="user-avatar">
+        <span id="user-name" class="user-name">Meminator User</span>
+    </div>
+    '''
+
+    # Return the rendered template
+    return render_template_string(user_info_template)
 
 if __name__ == '__main__':
     app.run(debug=True, port=10115)
