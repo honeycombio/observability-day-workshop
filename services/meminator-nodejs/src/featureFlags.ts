09c31d0d1f18b52c81dd104ef063aabf50b77356
Jessica Kerr <jessitron@honeycomb.io>
2025-04-16 19:45:50 -0600

    Remove feature flag bit
    It distracts from the check-text-width story, which is more interesting

--- services/meminator-nodejs/src/featureFlags.ts
+++ services/meminator-nodejs/src/featureFlags.ts
@@ -2,7 +2,7 @@ import { trace } from '@opentelemetry/api';
 
 export class FeatureFlags {
     useLibrary(): boolean {
-        const value = Math.random() < 0; // < 0.25;
+        const value = Math.random() < 0.25;
         trace.getActiveSpan()?.setAttribute('app.featureFlag.useLibrary', value);
         return value;
     }
